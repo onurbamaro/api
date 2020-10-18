38e8b4f45fe74ca90b1d4149b34a8a3abf52971f
Bruno Amaro <bruno.amaro@Live.com>
2020-10-17 23:50:54 -0300

    mostrar para o Carlos

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
+  title = 'Teste Carlos',
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
